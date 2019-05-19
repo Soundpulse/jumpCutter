91a17e6486fc53e124663c4242f864d83a96b967
soundpulse <toby.io@outlook.com>
2019-05-19 23:58:40 +0100

    Added: phase vocoder
    Very Dirty Code. Will fix asap

--- audio-cutter.py
+++ audio-cutter.py
@@ -1,7 +1,10 @@
-import os
-from pydub import AudioSegment
+from moviepy.editor import VideoFileClip, AudioFileClip, vfx, concatenate_videoclips
 from pydub.silence import detect_silence, detect_nonsilent
-from moviepy.editor import VideoFileClip, vfx, concatenate_videoclips
+from pydub import AudioSegment
+from audiotsm import phasevocoder
+from audiotsm.io.wav import WavReader, WavWriter
+import shutil
+import os
 
 # 0. ARGUMENTS
 
@@ -9,15 +12,6 @@ SILENT_THRESHOLD = -50
 SOUNDED_SPEED = 2
 SILENT_SPEED = 8
 MIN_SILENCE_LENGTH = 1000
-
-
-def speed_change(audio, speed=1.0):
-    # Manually override the frame_rate. This tells the computer how many samples to play per second
-    sound_with_altered_frame_rate = audio._spawn(audio.raw_data, overrides={
-        "frame_rate": int(audio.frame_rate * speed)
-    })
-    return sound_with_altered_frame_rate.set_frame_rate(audio.frame_rate)
-    # Speed change adapted From stackoverflow.com/questions/51434897
 
 
 # 1. Change mp4 into audio
@@ -34,28 +28,50 @@ for video in os.listdir(currentPath):
 
         time_start = 0
         time_end = audio.duration_seconds
-        silent_ranges = detect_silence(audio, silence_thresh=SILENT_THRESHOLD,  min_silence_len=MIN_SILENCE_LENGTH)
-        non_silent_ranges = detect_nonsilent(audio, silence_thresh=SILENT_THRESHOLD,
+        silent_chunks = detect_silence(audio, silence_thresh=SILENT_THRESHOLD,  min_silence_len=MIN_SILENCE_LENGTH)
+        non_silent_chunks = detect_nonsilent(audio, silence_thresh=SILENT_THRESHOLD,
                                              min_silence_len=MIN_SILENCE_LENGTH)
 
         # Used to distinguish silence and sounded
-        for array in silent_ranges:
+        for array in silent_chunks:
             array.append(1)
-        for array in non_silent_ranges:
+        for array in non_silent_chunks:
             array.append(0)
 
-        ranges = silent_ranges + non_silent_ranges
-        ranges.sort()
+        chunks = silent_chunks + non_silent_chunks
+        chunks.sort()
 
         clips = []
-        for i_start, i_end, silence in ranges:
+        os.mkdir(currentPath + "\\temp")
+        i = 0
+        for i_start, i_end, silence in chunks:
+            i += 1
+            if silence == 0:
+                speed = SOUNDED_SPEED
+            else:
+                speed = SILENT_SPEED
+
             sub_clip = clip.subclip(i_start/1000, i_end/1000)
 
-            if silence == 0:
-                sub_clip = sub_clip.fx(vfx.speedx, SOUNDED_SPEED)
-            else:
-                sub_clip = sub_clip.fx(vfx.speedx, SILENT_SPEED)
+            sub_clip.write_videofile(currentPath + "\\temp\\sub_clip{0}.mp4".format(i))
+            AudioSegment.from_file(currentPath + "\\temp\\sub_clip{0}.mp4".format(i)
+                                   ).export(currentPath + "\\temp\\sub_clip{0}.wav".format(i), format='wav')
+
+            src = currentPath + "\\temp\\sub_clip{0}.wav".format(i)
+            out = currentPath + "\\temp\\sub_clip-reg{0}.wav".format(i)
+            with WavReader(src) as reader:
+                with WavWriter(out, reader.channels, reader.samplerate) as writer:
+                    tsm = phasevocoder(reader.channels, speed=speed)
+                    tsm.run(reader, writer)
+
+            sub_clip = sub_clip.fx(vfx.speedx, speed)
+            sub_clip = sub_clip.set_audio(AudioFileClip(out))
+            sub_clip.write_videofile(currentPath + "\\temp\\sub_clip-reg{0}.mp4".format(i))
+
             clips.append(sub_clip)
 
         output_clip = concatenate_videoclips(clips)
         output_clip.write_videofile(currentPath + "\\output.mp4")
+
+        # DANGEROUS
+        shutil.rmtree(currentPath + "\\temp")
